f3f8c2933650d1fad2d77065b5d80d2cf3debf4c
SyedSameerShah <96075623+SyedSameerShah@users.noreply.github.com>
2024-03-04 20:49:31 +0530

    Update settings.py

--- HelloWorldSocketServer/settings.py
+++ HelloWorldSocketServer/settings.py
@@ -22,10 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ucw*%uml@)of%gis7#dk2vodme0c8yg@!_cpg-vw&2mp$m@9w_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.235.73.225']
 
 
 # Application definition
@@ -128,6 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-
 
 
